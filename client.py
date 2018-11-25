59640a43bfc9d1e0f2a8119cbd4295dfde6a8c6e
Pranav Bhaskar <pranavbhaskar23@gmail.com>
2018-11-25 05:03:54 +0000

    Add files via upload

---
+++ client.py
@@ -0,0 +1,74 @@
+import socket
+import cv2
+import _thread
+import pickle
+import time
+
+IP = input('Enter IP : ')
+PORT = 8888
+
+class connection_clientside():
+	def __init__(self, s):
+		self.link = s
+		self.reciving_flag = False
+	
+	def auth(self):
+		data = input('Enter Password : ').strip()
+		self.link.sendall(data.encode('utf-8'))
+	
+	def setter(self):
+		self.link.sendall('1'.encode('utf-8'))
+		self.link.recv(1024)
+	
+	def get(self):
+		try:
+			s.sendall('69'.encode('utf-8'))
+			data_len = int(self.link.recv(1024).decode('utf-8'))
+			#print(data_len)
+			if data_len < 100:
+				print('error')
+				return
+			s.sendall('69'.encode('utf-8'))
+			data = b''
+			while len(data) < data_len:
+				block = self.link.recv(4096)
+				data += block
+			image = pickle.loads(data,encoding='bytes')
+			cv2.imwrite('open.png', image)
+			if not self.reciving_flag:
+				self.reciving_flag = True
+				print('Reciveving data from ' + IP) #+ self.ip)
+		except KeyboardInterrupt:
+			print('\nWill Begining Exitting')
+			raise
+		except:
+			print('Connection Problem\nExitting')
+			reciving_flag = False
+			raise
+		
+	def dis(self):
+		print('Closing Connection')
+		if self.reciving_flag:
+			self.reciving_flag = False
+			self.link.sendall('17'.encode('utf-8'))
+			self.link.close()
+
+s = socket.socket()
+
+s.connect((IP, PORT))
+
+con = connection_clientside(s)
+
+data = s.recv(1024)
+con.auth()
+data = s.recv(1024)
+print(data.decode())
+if data.decode('utf-8') is '2':
+	exit()
+con.setter()
+while True:
+	try:
+		con.get()
+	except:
+		break
+con.dis()
---
+++ server.py
@@ -0,0 +1,77 @@
+import socket
+import _thread
+import cv2
+import time
+import pickle
+
+PASSWORD = '123'
+IP = socket.gethostname()
+PORT = 8888
+
+'''
+	1 -> Authenticate
+	2 -> Authenticatication failed
+	0 -> Authenticatication done / Send environment
+	69 -> Over and out
+	17 -> End Connection
+'''
+
+class connection_serverside():
+	def __init__(self, c, md):
+		self.link = c
+		self.ip = md[0]
+		self.port = md[1]
+		self.sending_flag = False
+	
+	def auth(self):
+		self.link.send('1'.encode('utf-8'))
+		print('NEW USER ' + self.ip)
+		data = self.link.recv(1024)
+		print('User ' + self.ip + ' tried entering : ' + data.decode('utf-8'))
+		if PASSWORD == str(data.decode('utf-8')):
+			self.link.send('0'.encode('utf-8'))
+			self.setter()
+			return 1
+		self.link.send('2'.encode('utf-8'))
+		return 0
+	
+	def setter(self):
+		data = self.link.recv(1024)
+		self.link.send('2'.encode('utf-8'))
+		self.sleep_time = int(data.decode('utf-8'))
+	
+	def send(self):
+		data = self.link.recv(1024)
+		if '17' == str(data.decode('utf-8')):
+			return False
+		return_value, image = camera.read()
+		serialized_data = pickle.dumps(image, protocol=2)
+		serialized_data_len = str(len(serialized_data))
+		self.link.send(serialized_data_len.encode('utf-8'))
+		data = self.link.recv(1024)
+		if '17' == str(data.decode('utf-8')):
+			return False
+		self.link.send(serialized_data)
+		return True
+	
+	def sender(self):
+		while self.send():
+			if not self.sending_flag:
+				self.sending_flag = True
+				print('Senting data to ' + self.ip)
+		print('Connection closed with ' + self.ip)
+		c.close()
+
+s = socket.socket()
+
+s.bind((IP, PORT))
+
+s.listen(2)
+
+camera = cv2.VideoCapture(0)
+
+while True:
+	c, md = s.accept()
+	cont = connection_serverside(c, md)
+	if cont.auth():
+		_thread.start_new_thread(cont.sender, ())
